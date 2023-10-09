155e3080b1a1c86597e5e6bc81f08cf3ab5eadd6
Шкарёв Андрей <wex335@yandex.ru>
2023-10-09 09:54:11 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        public static void Main(){
+
+            Console.WriteLine("hello world");
+            Console.ReadKey();
+        }
+    }
+} 
